663c7a05e5511452dee2e92a8ed8ea6ec684d4bf
Yoch Melka <yoch.melka@gmail.com>
2026-05-26 04:34:41 +0300

    bugfix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,13 +4,9 @@ module.exports = {
       name: 'medicaments-api',
       script: 'src/server.js',
       node_args: '--env-file=.env',
-      max_memory_restart: '512M',
       env: {
         NODE_ENV: 'production',
         RELOAD_STRATEGY: 'restart'
-      },
-      env_production: {
-        NODE_OPTIONS: '--max-old-space-size=512'
       }
     }
   ]
