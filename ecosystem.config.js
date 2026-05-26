99eef86cf571aa38edae1d2c7b7ad8bc3d5b0450
Yoch Melka <yoch.melka@gmail.com>
2026-05-26 11:50:48 +0300

    minor

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,8 @@ module.exports = {
     {
       name: 'medicaments-api',
       script: 'src/server.js',
+      time: true,
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
       node_args: '--env-file=.env',
       env: {
         NODE_ENV: 'production',
